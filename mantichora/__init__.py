391f61cbc8493a6f70e984cc4d91602e23cc3ed8
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-28 14:03:20 +0000

    use versioneer

--- mantichora/__init__.py
+++ mantichora/__init__.py
@@ -1,1 +1,5 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import versioneer
+
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mantichora',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='A simple interface to threading and multiprocessing',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tai Sakuma',
+    author_email='tai.sakuma@gmail.com',
+    url='https://github.com/alphatwirl/mantichora',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'atpbar>=0.9.7'
+    ]
+)
