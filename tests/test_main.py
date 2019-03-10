0e5dc4d71abb8593a0565345dbf6e490fbda00f1
Tai Sakuma <tai.sakuma@gmail.com>
2019-03-10 10:18:43 +0000

    add comment to test_with_terminate()

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,6 +35,10 @@ def test_with_terminate():
         mcore.run(task, 12, 'result 2')
         mcore.run(task, 15, 'result 3')
 
+        # Since `mcore.returns()` or any methods that wait for the
+        # tasks to finish are called, the `with` statement will exit
+        # quickly, at which the tasks will be terminated.
+
 ##__________________________________________________________________||
 class MyException(Exception):
     pass
