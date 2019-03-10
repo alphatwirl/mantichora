62ba134e17c0293594f37fe5f7257a9908ca0be2
Tai Sakuma <tai.sakuma@gmail.com>
2019-03-10 11:43:39 +0000

    split test_MultiprocessingDropbox.py into two files

--- tests/test_MultiprocessingDropbox_poll_receive.py
+++ tests/test_MultiprocessingDropbox_poll_receive.py
@@ -14,26 +14,6 @@ from mantichora.hub import MultiprocessingDropbox
 from mantichora.hub import TaskPackage
 
 ##__________________________________________________________________||
-def test_init_raise():
-    with pytest.raises(ValueError):
-        MultiprocessingDropbox(nprocesses=0)
-
-def test_open_close():
-    obj = MultiprocessingDropbox()
-    obj.open()
-    obj.close()
-
-def test_open_open_close():
-    obj = MultiprocessingDropbox()
-    obj.open()
-    obj.open() # don't create workers again
-    obj.close()
-
-def test_repr():
-    obj = MultiprocessingDropbox()
-    repr(obj)
-
-##__________________________________________________________________||
 MockResult = collections.namedtuple('MockResult', 'name args kwargs')
 
 class MockTask(object):
@@ -84,83 +64,85 @@ def obj():
     ret.close()
 
 ##__________________________________________________________________||
-def test_put(obj, package1, package2):
-    assert 0 == obj.put(package1)
-    assert 1 == obj.put(package2)
-
-def test_put_multiple(obj, package1, package2):
-    assert [0, 1] == obj.put_multiple([package1, package2])
-
-def test_put_receive(obj, package1, package2):
-    packages = [package1, package2]
-    pkgidxs = [ ]
-    for p in packages:
-        pkgidxs.append(obj.put(p))
+def test_poll(obj, package1, package2, package3, package4):
+    packages = [package1, package2, package3, package4]
+    pkgidxs = obj.put_multiple(packages)
 
     expected = [
         (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
         for i, p in zip(pkgidxs, packages)]
-    actual = obj.receive()
-    assert expected == actual
 
-def test_receive_order(obj, package1, package2, package3):
-    # results of tasks are sorted in the order in which the tasks are put.
+    actual = [ ]
+    while len(actual) < len(expected):
+        actual.extend(obj.poll())
+    assert sorted(expected) == sorted(actual)
 
-    packages = [package1, package2, package3]
-    pkgidxs = [ ]
-    for p in packages:
-        pkgidxs.append(obj.put(p))
-
-    expected = [
-        (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
-        for i, p in zip(pkgidxs, packages)]
-    actual = obj.receive()
-    assert expected == actual
-
-def test_put_receive_repeat(obj, package1, package2, package3, package4):
-
-    packages = [package1, package2]
-    pkgidxs = [ ]
-    for p in packages:
-        pkgidxs.append(obj.put(p))
+def test_poll_then_receive(obj, package1, package2, package3, package4):
+    packages = [package1, package2, package3, package4]
+    pkgidxs = obj.put_multiple(packages)
 
     expected = [
         (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
         for i, p in zip(pkgidxs, packages)]
-    actual = obj.receive()
-    assert expected == actual
 
-    packages = [package3, package4]
-    pkgidxs = [ ]
-    for p in packages:
-        pkgidxs.append(obj.put(p))
+    actual = [ ]
+    while not actual:
+        actual.extend(obj.poll())
+
+    actual.extend(obj.receive())
+
+    assert sorted(expected) == sorted(actual)
+
+##__________________________________________________________________||
+def test_receive_one(obj, package1, package2, package3, package4):
+    packages = [package1, package2, package3, package4]
+    pkgidxs = obj.put_multiple(packages)
 
     expected = [
         (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
         for i, p in zip(pkgidxs, packages)]
-    actual = obj.receive()
-    assert expected == actual
 
-def test_begin_put_recive_end_repeat(obj, package1, package2):
-    obj.put(package1)
-    obj.receive()
-    obj.close()
-    obj.open()
-    obj.put(package2)
-    obj.receive()
+    actual = [ ]
+    actual.append(obj.receive_one())
+    actual.append(obj.receive_one())
+    actual.append(obj.receive_one())
+    actual.append(obj.receive_one())
+    assert obj.receive_one() is None
+    assert sorted(expected) == sorted(actual)
 
-def test_terminate(obj, package1, package2):
-    obj.put(package1)
-    obj.put(package2)
-    obj.terminate()
+def test_receive_one_then_receive(obj, package1, package2, package3, package4):
+    packages = [package1, package2, package3, package4]
+    pkgidxs = obj.put_multiple(packages)
 
-def test_terminate_close(obj, package1, package2):
-    obj.put(package1)
-    obj.put(package2)
-    obj.terminate()
-    obj.close()
+    expected = [
+        (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
+        for i, p in zip(pkgidxs, packages)]
 
-def test_receive_without_put(obj):
-    assert [ ] == obj.receive()
+    time.sleep(0.005) # so multiple jobs finish
+
+    actual = [ ]
+    actual.append(obj.receive_one())
+
+    actual.extend(obj.receive())
+
+    assert sorted(expected) == sorted(actual)
+
+def test_receive_one_then_poll(obj, package1, package2, package3, package4):
+    packages = [package1, package2, package3, package4]
+    pkgidxs = obj.put_multiple(packages)
+
+    expected = [
+        (i, MockResult(name=p.task.name, args=p.args, kwargs=p.kwargs))
+        for i, p in zip(pkgidxs, packages)]
+
+    time.sleep(0.005) # so multiple jobs finish
+
+    actual = [ ]
+    actual.append(obj.receive_one())
+
+    while len(actual) < len(expected):
+        actual.extend(obj.poll())
+
+    assert sorted(expected) == sorted(actual)
 
 ##__________________________________________________________________||
