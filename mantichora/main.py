1b719be27a799ee0f0bdec6c51276398cc1f724f
Tai Sakuma <tai.sakuma@gmail.com>
2019-03-14 04:17:58 +0000

    add __call__(), docstring to TaskPackage

--- mantichora/main.py
+++ mantichora/main.py
@@ -5,10 +5,17 @@ from .hub import MultiprocessingDropbox
 
 ##__________________________________________________________________||
 class TaskPackage(object):
+    """A task package
+
+    Note: This class will be replaced with `functools.partial`
+
+    """
     def __init__(self, task, args, kwargs):
         self.task = task
         self.args = args
         self.kwargs = kwargs
+    def __call__(self):
+        return self.task(*self.args, **self.kwargs)
 
 ##__________________________________________________________________||
 class mantichora(object):
