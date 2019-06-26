364121c8ca4f16b914803329310e18ad5d65699d
Tai Sakuma <tai.sakuma@gmail.com>
2019-06-26 11:01:43 +0100

    move Worker to hubmp.py

--- mantichora/hubmp.py
+++ mantichora/hubmp.py
@@ -6,9 +6,12 @@ import threading
 
 from operator import itemgetter
 
-import atpbar
+try:
+    from logging.handlers import QueueHandler
+except ImportError:
+    from .queuehandler import QueueHandler
 
-from .worker import Worker
+import atpbar
 
 ##__________________________________________________________________||
 # https://docs.python.org/3/howto/logging-cookbook.html#logging-to-a-single-file-from-multiple-processes
@@ -227,5 +230,44 @@ class MultiprocessingHub(object):
 
         if self.progressbar:
             atpbar.flush()
+
+##__________________________________________________________________||
+class Worker(multiprocessing.Process):
+    def __init__(self, task_queue, result_queue, logging_queue,
+                 lock, progress_reporter):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+        self.logging_queue = logging_queue
+        self.lock = lock
+        self.progress_reporter = progress_reporter
+
+    def run(self):
+        self._configure_logger()
+        self._configure_progressbar()
+        try:
+            self._run_tasks()
+        except KeyboardInterrupt:
+            pass
+
+    def _configure_logger(self):
+        handler = QueueHandler(self.logging_queue)
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+
+    def _configure_progressbar(self):
+        atpbar.register_reporter(self.progress_reporter)
+
+    def _run_tasks(self):
+        while True:
+            message = self.task_queue.get()
+            if message is None:
+                self.task_queue.task_done()
+                break
+            task_idx, task_func = message
+            result = task_func()
+            self.task_queue.task_done()
+            self.result_queue.put((task_idx, result))
 
 ##__________________________________________________________________||
