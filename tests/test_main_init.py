52c483820e437d492caf89bea51e58be22e9f0d8
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-28 19:10:09 -0400

    update tests for init

--- tests/test_main_init.py
+++ tests/test_main_init.py
@@ -4,14 +4,23 @@ import pytest
 from mantichora import mantichora
 
 ##__________________________________________________________________||
-def test_init_raise():
-    with pytest.raises(ValueError):
-        mantichora(mp_start_method='no-such-method')
-
-##__________________________________________________________________||
 @pytest.mark.parametrize('mp_start_method', ['fork', 'spawn', 'forkserver'])
 def test_init_mp_start_method(mp_start_method):
     with mantichora(mp_start_method=mp_start_method):
         pass
+
+def test_init_mp_start_method_raise():
+    with pytest.raises(ValueError):
+        mantichora(mp_start_method='no-such-method')
+
+##__________________________________________________________________||
+@pytest.mark.parametrize('mode', ['multiprocessing', 'threading'])
+def test_init_mode(mode):
+    with mantichora(mode=mode):
+        pass
+
+def test_init_mode_raise():
+    with pytest.raises(ValueError):
+        mantichora(mode='no-such-mode')
 
 ##__________________________________________________________________||
