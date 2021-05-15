524a553f142a4f34d623adf95041fa459b88534d
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-15 17:58:32 -0400

    skip test_run_example.py on Windows

--- tests/test_run_example.py
+++ tests/test_run_example.py
@@ -4,12 +4,18 @@
 """
 
 import os
+import platform
 
 import pytest
 
 here = os.path.abspath(os.path.dirname(__file__))
 example_dir = os.path.abspath(os.path.join(here, '..', 'examples'))
 example_script_path = os.path.join(example_dir, 'example_01.py')
+
+
+##__________________________________________________________________||
+system = platform.system().lower()
+pytestmark = pytest.mark.skipif(system=="windows", reason="get stuck in GitHub Actions")
 
 ##__________________________________________________________________||
 @pytest.mark.script_launch_mode('subprocess')
